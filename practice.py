7e5b0442ae85f410707c6b0e97ca0c39ecad2c18
Younghoon <gcloud1025@gmail.com>
2022-02-05 19:08:29 +0900

    0205

--- practice.py
+++ practice.py
@@ -1,9 +1,14 @@
-T = int(input())
-for i in range(T):
-    k = int(input())
-    n = int(input())
-    lst = [x for x in range(1, n+1)]
-    for i in range(k):
-        for i in range(1, n):
-            lst[i] += lst[i-1]
-    print(lst[-1])
+def isPrime(num):
+    if num == 1: # 1은 어차피 소수가 아니니까 취급 X
+        return False
+    else:
+        for i in range(2, int(num**0.5) + 1): # num을 나누어줄 range는 2부터 num의 제곱근까지
+            if num % i == 0:
+                return False
+        return True
+    
+M, N = map(int, input().split())
+
+for i in range(M, N, +1):
+    if isPrime(i): # isPrime(i)의 결과가 True라면
+        print(i)
