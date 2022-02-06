b227fc42d439b1b23f9613d9b87c37947d28b1f7
Younghoon <gcloud1025@gmail.com>
2022-02-06 23:47:05 +0900

    0206

--- practice.py
+++ practice.py
@@ -1,14 +1,12 @@
-def isPrime(num):
-    if num == 1: # 1은 어차피 소수가 아니니까 취급 X
-        return False
-    else:
-        for i in range(2, int(num**0.5) + 1): # num을 나누어줄 range는 2부터 num의 제곱근까지
-            if num % i == 0:
-                return False
-        return True
-    
-M, N = map(int, input().split())
+import sys
 
-for i in range(M, N, +1):
-    if isPrime(i): # isPrime(i)의 결과가 True라면
-        print(i)
+while 1:
+    try:
+        x1 = list(map(int, sys.stdin.readline().split()))
+        x2 = x1.sort()
+        if x2[0]**2 + x2[1]**2 == x2[2]**2:
+            print('right')
+        else:
+            print('wrong')
+    except:
+        break
