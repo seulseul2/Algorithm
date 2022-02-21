913d89573bf732475b6f2ea3c7722b5e336df243
Younghoon <gcloud1025@gmail.com>
2022-02-22 01:17:30 +0900

    0222

--- practice.py
+++ practice.py
@@ -1,11 +1,10 @@
-arr = [[9, 20, 2, 18, 11], [19, 1, 25, 3, 21] ,[8, 24, 10, 17, 7], [15, 4, 16, 5, 6], [12, 13, 22, 23, 14]]
-
-I = len(arr)
-J = len(arr[0])
-
-mindata = 100000
-for i in range(I):
-    for j in range(J):
-        if arr[i][j] < mindata:
-            mindata = arr[i][j]
-print(mindata)    
+N = int(input())
+for i in range(6):
+    lst = [] # 방향 리스트
+    dict = {} # 거리 리스트
+    direction, distance = map(int, input().split())
+    if direction not in lst:
+        lst.append(direction)
+    else:
+        if direction == 1:
+            if 2 in lst:
--- practice2.py
+++ practice2.py
@@ -1,10 +1,18 @@
-lst = list(map(int, input().split()))
+A, B = map(int, input().split())
+cnt = 1
+while B > A:
+    if str(B)[-1] == '1':
+        B = int(str(B)[:-1])
+        cnt += 1
+        continue
+    elif B % 2 == 0:
+        B //= 2
+    else:
+        cnt = -1
+        break
+    cnt += 1
 
-n = len(lst)
-
-for i in range(1<<n): # 0 ~ 1023까지
-    for j in range(n): # j는 0~9까지
-        if i & (1<<j): # 만약에 0~1023과 1<<j 사이 부분집합이 있다면
-            print(lst[j], end=' ') # j를 출력해라~ 뭐 그런뜻인거같은데 시 팔
-    print()
-print()
+if A != B:
+    cnt = -1
+    
+print(cnt)
