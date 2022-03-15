c40c8cf81d74ad0f965ad3b784a81a2a288b2098
Younghoon <gcloud1025@gmail.com>
2022-03-15 12:44:29 +0900

    0315

--- practice.py
+++ practice.py
@@ -1,46 +1,31 @@
-import sys
-input = sys.stdin.readline
+T = int(input())
+for TC in range(1, T+1):
+    N, M = map(int, input().split())
+    cheese = list(map(int, input().split()))
+    queue = []
+    num = 0
+    cnt = 0
 
-N, M, V = map(int, input().split())
-arr = [[] for _ in range(N+1)]
-for i in range(M):
-    a, b = map(int, input().split())
-    arr[a].append(b)
-    arr[b].append(a)
+    # 화덕에 피자 넣기
+    for i in range(N):
+        num += 1
+        queue.append([num, cheese[num-1]])
 
+    # 계속해서 돌려주기, 반복
+    while 1:
+        queue[0][1] = (queue[0][1] // 2)
+        if queue[0][1] == 0:
+            cnt += 1
+            if cnt == M:
+                result = queue[0][0]
+                break
+            num += 1
+            if num <= M:
+                queue[0][0] = num
+                queue[0][1] = cheese[num-1]
+            else:
+                queue.pop(0)
+                continue
+        queue.append(queue.pop(0))
 
-# 핵심.
-# for j in range(1, N+1):
-#     arr[j].sort()
-
-print(arr)
-# def dfs(s):
-#     stack = [s]
-#     visited = [False] * (N+1)
-#     lst = []
-#     while stack:
-#         start = stack.pop()
-#         if not visited[start]:
-#             visited[start] = True
-#             lst.append(start)
-#             for e in arr[start][::-1]:
-#                 if not visited[e]:
-#                     stack.append(e)
-#     return lst
-
-# def bfs(s):
-#     queue = [s]
-#     visited = [False] * (N+1)
-#     result = []
-#     while queue:
-#         start = queue.pop(0)
-#         if not visited[start]:
-#             visited[start] = True
-#             result.append(start)
-#             for e in arr[start]:
-#                 if not visited[e]:
-#                     queue.append(e)
-#     return result
-
-# print(*dfs(V))
-# print(*bfs(V))
+    print('#{} {}' .format(TC, result))
