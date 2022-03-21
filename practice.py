6e464e9b4717a18f26dff91aee77bb7fbffa9eb5
Younghoon <gcloud1025@gmail.com>
2022-03-21 23:29:21 +0900

    0321

--- practice.py
+++ practice.py
@@ -1,40 +1,20 @@
-def preorder(root):
-    #1. root 처리
-    print(root, end=' ')
-    #2. 왼쪽 서브트리 처리
-    if len(maps[root]) > 0:
-        preorder(maps[root][0])
-    #3. 오른쪽 서브트리 처리
-    if len(maps[root]) > 1:
-        preorder(maps[root][1])
+import sys
+from itertools import permutations
+input = sys.stdin.readline
 
-def inorder(root):
-    if len(maps[root]) > 0:
-        inorder(maps[root][0])
-    print(root, end=' ')
-    if len(maps[root]) > 1:
-        inorder(maps[root][1])
+n, k = map(int, input().split())
+wod = list(map(int, input().split()))
+cnt = 0
 
-def postorder(root):
-    if len(maps[root]) > 0:
-        postorder(maps[root][0])
-    if len(maps[root]) > 1:
-        postorder(maps[root][1])
-    print(root, end=' ')
-
-N = 13
-inStr = '1 2 1 3 2 4 3 5 3 6 4 7 5 8 5 9 6 10 6 11 7 12 11 13'
-lst = list(map(int, inStr.split()))
-maps = [[] for _ in range(N+1)]
-
-for i in range(0, len(lst), 2):
-    p = lst[i]
-    c = lst[i+1]
-    maps[p].append(c)
-
-print(maps)
-# preorder(1)
-# print()
-# inorder(1)
-# print()
-# postorder(1)
+for data in permutations(wod, n):
+    weight = 500
+    flag = True
+    for i in range(n):
+        weight += data[i]
+        weight -= k
+        if weight < 500:
+            flag = False
+            break
+    if flag:
+        cnt += 1
+print(cnt)
