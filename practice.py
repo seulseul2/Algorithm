70ccb7997fa646637dee0064fb538b23148bd3d8
Younghoon <gcloud1025@gmail.com>
2022-04-09 22:08:38 +0900

    0409

--- practice.py
+++ practice.py
@@ -1,13 +1,44 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+n = int(input())
+maps = [list(input().rstrip()) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
 
-N = int(input())
-dp = [0] * 501
-for i in range(N):
-    lst = list(map(int, input().split()))
-    tmp = dp[:]
-    dp[0] += lst[0]
-    for j in range(1, len(lst)):
-        dp[j] = lst[j] + max(tmp[j-1], tmp[j])
-print(max(dp))
+dY = [-1, 1, 0, 0]
+dX = [0, 0, -1, 1]
+
+def dfs(y, x):
+    standard = maps[y][x]
+    stack = [[y, x]]
+    visited[y][x] = True
+    if maps[y][x] == standard:
+                visited[y][x] = True
+                stack = [[y, x]]
+                while stack:
+                    cY, cX = stack.pop()
+                    for i in range(4):
+                        nY, nX = cY+dY[i], cX+dX[i]
+                        if 0<=nY<n and 0<=nX<n and not visited[nY][nX] and maps[nY][nX] == standard:
+                            visited[nY][nX] = True
+                            stack.append([nY, nX])
+c1 = 0
+
+for y in range(n):
+    for x in range(n):
+        if not visited[y][x]:
+            c1 += 1
+            dfs(y, x)
+
+for y in range(n):
+    for x in range(n):
+        if maps[y][x] == 'R':
+            maps[y][x] = 'G'
+
+visited = [[False] * n for _ in range(n)]
+c2 = 0
+for y in range(n):
+    for x in range(n):
+        if not visited[y][x]:
+            c2 += 1
+            dfs(y, x)
+print(c1, c2)
