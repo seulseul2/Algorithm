b88b7fc0d1495ee715dd982859ab87ab2f2537c4
Younghoon <gcloud1025@gmail.com>
2022-04-14 22:40:14 +0900

    0414

--- practice.py
+++ practice.py
@@ -1,21 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
-for TC in range(T):
-    N = int(input())
-    result = N
-    lst = []
+N, M, K = map(int, input().split())
+maps = [[0] * M for _ in range(N)]
+maps[0][0] = 1
+a = 1
+if K != 0:
+    Ky = (K-1)//M
+    Kx = (K-1) % M
+    for i in range(Ky+1):
+        for j in range(Kx+1):
+            if i == 0 or j == 0:
+                maps[i][j] = 1
+            else:
+                maps[i][j] = maps[i-1][j] + maps[i][j-1]
+    a, maps[Ky][Kx] = maps[Ky][Kx], a
+    for i in range(Ky, N):
+        for j in range(Kx, M):
+            if i == Ky or j == Kx:
+                maps[i][j] = 1
+            else:
+                maps[i][j] = maps[i-1][j] + maps[i][j-1]
+    b = maps[N-1][M-1]
+else:
     for i in range(N):
-        resume, interview = map(int, input().split())
-        lst.append([resume, interview])
-    lst.sort()
-    standard = lst[0]
-    for applicant in lst[1:]:
-        # 기준 변경
-        if standard[1] > applicant[1]:
-            standard = applicant
-            continue
-        # 면접점수도 낮은 경우
-        result -= 1
-    print(result)
+        for j in range(M):
+            if i == 0 or j == 0:
+                maps[i][j] = 1
+            else:
+                maps[i][j] += maps[i-1][j] + maps[i][j-1]
+b = maps[N-1][M-1]
+print(a*b)
