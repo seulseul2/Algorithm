0e84709ca72f1afbdc0f704fbe06131ddcc036d6
Younghoon <gcloud1025@gmail.com>
2022-04-21 22:35:37 +0900

    0421

--- 모의A 2번.py
+++ 모의A 2번.py
@@ -1,33 +1,16 @@
-def dfs(y, x):
-    stack = [[y, x]]
-    visited = [[False] * M for _ in range(N)]
-    visited[y][x] = True
-    while stack:
-        cY, cX = stack.pop()
-        for i in range(X):
-            nY, nX = cY+dY[i], cX+dX[i]
-            if 0<=nY<N and 0<=nX<M and maps[nY][nX] != 0 and not visited[nY][nX]:
-                if maps[nY][nX] == 3:
-                    return True
-                stack.append([nY, nX])
-                visited[nY][nX] = True
-    return False
+import heapq
+import sys
+input = sys.stdin.readline
 
-T = int(input())
-for TC in range(1, T+1):
-    dY = [0, 0]
-    dX = [-1, 1]
-    N, M = map(int, input().split())
-    maps = [list(map(int, input().split())) for _ in range(N)]
-    jump = 0
-    X = 2
-    while jump < 50:
-        jump += 1
-        X += 2
-        dY.append(jump)
-        dY.append(-jump)
-        dX.append(0)
-        dX.append(0)
-        if dfs(N-1, 0):
-            print('#{} {}' .format(TC, jump))
-            break
+N = int(input())
+card = []
+for i in range(N):
+    heapq.heappush(card, int(input()))
+
+total = 0
+for j in range(N-1):
+    x = heapq.heappop(card) + heapq.heappop(card)
+    heapq.heappush(card, x)
+    total += x
+
+print(total)
