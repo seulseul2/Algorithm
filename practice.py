8c852088b7ed2a6ae6fc8e7a4621d8264669cb6c
Younghoon <gcloud1025@gmail.com>
2022-06-17 20:50:17 +0900

    0617

--- practice.py
+++ practice.py
@@ -1,15 +1,33 @@
+# import sys
+# input = sys.stdin.readline
+
+# N, M = map(int, input().split())
+# lst = []
+
+# def btrk(x):
+#     if len(lst) == M:
+#         print(' '.join(map(str, lst)))
+#         return
+#     for i in range(x, N+1):
+#         lst.append(i)
+#         btrk(i)
+#         lst.pop()
+
+# btrk(1)
+
 import sys
 input = sys.stdin.readline
 
-T = int(input())
-for TC in range(T):
-    M, N, x, y = map(int, input().split())
-    while x != y:
-        if x > y:
-            y += N
-        else:
-            x += M
-        if x > N*M or y > N*M:
-            x = -1
-            break
-    print(x)
+N, M = map(int, input().split())
+lst = []
+
+def btrk(x):
+    if len(lst) == M:
+        print(*lst)
+        return
+    for i in range(x, N+1):
+        lst.append(i)
+        btrk(i)
+        lst.pop()
+
+btrk(1)
