dde98fe2d3395bc26725331296c47f131d87929a
Younghoon <gcloud1025@gmail.com>
2022-06-23 21:33:57 +0900

    0623

--- practice.py
+++ practice.py
@@ -1,15 +1,11 @@
-import sys
-input = sys.stdin.readline
+INF = 5001
+n = int(input())
+room = list(map(int, input().split()))
+m = int(input())
+dp = [-INF for _ in range(m+1)]
+for i in range(n-1, -1, -1):
+    x = room[i]
+    for j in range(x, m+1):
+        dp[j] = max(dp[j-x]*10+i, i, dp[j])
 
-M, N = map(int, input().split())
-pasture = [list(map(int, input().split())) for _ in range(M)]
-dp = [[0] * (N+1) for _ in range(M+1)]
-maxV = 0
-
-for i in range(1, M+1):
-    for j in range(1, N+1):
-        if not pasture[i-1][j-1]:
-            dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
-            maxV = max(maxV, dp[i][j])
-print(dp)
-print(maxV)
+print(dp[m])
