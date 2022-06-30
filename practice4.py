d0121fb82ce7ed5ebd9ed690ac95dd8fb54c2ee2
Younghoon <gcloud1025@gmail.com>
2022-06-30 21:48:05 +0900

    0630

--- practice4.py
+++ practice4.py
@@ -1,23 +1,2 @@
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-liquid = list(map(int, input().split()))
-liquid.sort()
-
-minV = 2000000001
-left, right = 0, N-1
-
-while left < right:
-    tmp = liquid[left]+liquid[right]
-    if abs(tmp) < minV:
-        a = liquid[left]
-        b = liquid[right]
-        minV = abs(tmp)
-        if minV == 0:
-            break
-    if tmp > 0:
-        right -= 1
-    else:
-        left += 1
-print(a, b)
+x = '1 2+3-4'
+print(eval(x.replace(' ','')))
