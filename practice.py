738412617c0a97cdbbefb1bf6c57709f18e05cb4
Younghoon <gcloud1025@gmail.com>
2022-07-04 17:06:01 +0900

    0704

--- practice.py
+++ practice.py
@@ -1,24 +1,13 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-INF = 100001
+coins = [int(input()) for _ in range(N)]
+dp = [0] * (K+1)
+dp[0] = 1
 
-q = deque([N])
-cnt = 0
-visited = [-1] * INF
-visited[N] = 0
-
-while q:
-    now = q.popleft()
-    if now==K:
-        cnt+=1
-    for next in [now*2, now+1, now-1]:
-        if 0<=next<INF:
-            if visited[next]==-1 or visited[next]>=visited[now]+1: #시간: 방문한 적 없거나 현재시간 +1인경우
-                visited[next]=visited[now]+1
-                q.append(next)
-
-print(visited[K])
-print(cnt)
+for coin in coins:
+    for i in range(coin, K+1):
+        if i>=coin:
+            dp[i] += dp[i-coin]
+print(dp[K])
